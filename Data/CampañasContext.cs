d703ad00857d2089e9250c4cd4f943ad9ccc5c9d
batusaijb <42442877+batusaijb@users.noreply.github.com>
2019-11-03 18:11:22 -0500

    Add files via upload

---
+++ Data/CampañasContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Campañas.Models
+{
+    public class CampañasContext : DbContext
+    {
+        public CampañasContext (DbContextOptions<CampañasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Campañas.Models.Campaña> Campaña { get; set; }
+    }
+}
